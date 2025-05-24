63fb7cf84dfefcf861d774a6949cc7b6060b2bbf
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2025-05-24 16:44:52 +0200

    add NoOvertestingMultiFrameScreenshotTest for the first code challenge

---
+++ source-code/final-project/paparazzi-tests/src/test/java/com/droidcon/flavorshub/paparazzi/screenshots/NoOvertestingRecipeCardScreenshotTest.kt
@@ -0,0 +1,109 @@
+package com.droidcon.flavorshub.paparazzi.screenshots
+
+import androidx.compose.ui.Modifier
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams
+import com.android.resources.NightMode
+import com.droidcon.flavorshub.paparazzi.screenshots.testRules.FakeImageLoaderRule
+import com.droidcon.flavorshub.ui.model.FavoriteState
+import com.droidcon.flavorshub.ui.model.Type
+import com.droidcon.flavorshub.ui.screens.main.model.MainScreenRecipeItem
+import com.droidcon.flavorshub.ui.screens.main.ui.RecipeCard
+import com.droidcon.flavorshub.ui.theme.FlavorshubTheme
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Execute the following command for this screenshot
+ * Record:
+ * ./gradlew :paparazzi-tests:recordPaparazziDebug --tests 'NoOvertestingRecipeCardScreenshotTest'
+ * Verify:
+ * ./gradlew :paparazzi-tests:verifyPaparazziDebug --tests 'NoOvertestingRecipeCardScreenshotTest'
+ */
+@RunWith(TestParameterInjector::class)
+class NoOvertestingRecipeCardScreenshotTest(
+    @TestParameter val testCase: TestCase,
+) {
+
+    enum class RecipeInLocale(
+        val localeAsString: String,
+        val mainScreenRecipeItem: MainScreenRecipeItem
+    ) {
+        ENGLISH(
+            localeAsString = "en",
+            mainScreenRecipeItem = MainScreenRecipeItem(
+                id = 1,
+                name = "Recipe Title",
+                shortDescription = "Recipe Description",
+                type = Type.MEAT,
+                cookingTimeInMin = 30,
+                isFavorite = FavoriteState.FAVORITE,
+                imageUrl = "https://example.com/recipe-image.jpg"
+            )
+        ),
+        ARABIC(
+            localeAsString = "ar",
+            mainScreenRecipeItem = MainScreenRecipeItem(
+                id = 2,
+                name = "عنوان الوصفة",
+                shortDescription = "وصف الوصفة",
+                type = Type.VEGAN,
+                cookingTimeInMin = 30,
+                isFavorite = FavoriteState.NOT_FAVORITE,
+                imageUrl = "https://example.com/recipe-image.jpg"
+            )
+        )
+    }
+
+    enum class TestCase(
+        val device: DeviceConfig,
+        val fontSize: Float,
+        val recipeInLocale: RecipeInLocale,
+        val uiMode: NightMode
+    ) {
+        HAPPY_PATH_LARGE(
+            device = DeviceConfig.PIXEL_4,
+            fontSize = 1.25f,
+            recipeInLocale = RecipeInLocale.ENGLISH,
+            uiMode = NightMode.NOTNIGHT,
+        ),
+        UNHAPPY_PATH(
+            device = DeviceConfig.PIXEL_C,
+            fontSize = 1.0f,
+            recipeInLocale = RecipeInLocale.ARABIC,
+            uiMode = NightMode.NIGHT
+        )
+    }
+
+    @get:Rule(order = 0)
+    val paparazzi = Paparazzi(
+        deviceConfig = testCase.device.copy(
+            fontScale = testCase.fontSize,
+            nightMode = testCase.uiMode,
+            locale = testCase.recipeInLocale.localeAsString
+        ),
+        supportsRtl = true,
+        renderingMode = SessionParams.RenderingMode.SHRINK
+    )
+
+    @get:Rule(order = 1)
+    val fakeImageLoader = FakeImageLoaderRule { paparazzi.context }
+
+    @Test
+    fun recipeCardScreenshot() {
+        paparazzi.snapshot(name = "RecipeCard") {
+            FlavorshubTheme {
+                RecipeCard(
+                    modifier = Modifier.Companion,
+                    recipe = testCase.recipeInLocale.mainScreenRecipeItem,
+                    onFavouriteClick = { id -> },
+                    onClick = { }
+                )
+            }
+        }
+    }
+}
---
+++ source-code/final-project/roborazzi-tests/src/test/java/com/droidcon/flavorshub/roborazzi/screenshots/NoOvertestingMultiFrameScreenshotTest.kt
@@ -0,0 +1,146 @@
+package com.droidcon.flavorshub.roborazzi.screenshots
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import com.droidcon.flavorshub.FlavorshubActivity
+import com.droidcon.flavorshub.roborazzi.screenshots.interactor.ScreenInteractor
+import com.droidcon.flavorshub.roborazzi.screenshots.testrules.AddActivityToRobolectricRule
+import com.droidcon.flavorshub.roborazzi.screenshots.testrules.FakeImageLoaderRule
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziRule
+import com.github.takahirom.roborazzi.RoborazziRule.Options
+import com.google.testing.junit.testparameterinjector.TestParameter
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestParameterInjector
+import org.robolectric.RuntimeEnvironment.setFontScale
+import org.robolectric.RuntimeEnvironment.setQualifiers
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+/**
+ * Execute the following command for this screenshot
+ * Record:
+ * ./gradlew :roborazzi-tests:recordRoborazziDebug --tests 'NoOvertestingMultiFrameScreenshotTest'
+ * Verify:
+ * ./gradlew :roborazzi-tests:verifyRoborazziDebug --tests 'NoOvertestingMultiFrameScreenshotTest'
+ */
+@HiltAndroidTest
+@Config(application = HiltTestApplication::class)
+@RunWith(RobolectricTestParameterInjector::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+class NoOvertestingMultiFrameScreenshotTest(
+    @TestParameter val testCase: TestCase
+) {
+
+    enum class Device(val qualifier: String) {
+        TABLET(RobolectricDeviceQualifiers.PixelC),
+        PHONE(RobolectricDeviceQualifiers.Pixel4)
+    }
+
+    enum class UiMode(val cumulativeQualifier: String) {
+        LIGHT("+notnight"),
+        DARK("+night")
+    }
+
+    enum class FontSize(val scale: Float) {
+        NORMAL(1.0f),
+        LARGE(1.3f)
+    }
+
+    enum class Locale(val cumulativeQualifier: String) {
+        ENGLISH("+en"),
+        ARABIC("+ar")
+    }
+
+    enum class TestCase(
+        val device: Device,
+        val fontSize: FontSize,
+        val locale: Locale,
+        val uiMode: UiMode
+    ) {
+        HAPPY_PATH_LARGE(
+            device = Device.PHONE,
+            fontSize = FontSize.LARGE,
+            locale = Locale.ENGLISH,
+            uiMode = UiMode.LIGHT
+        ),
+        UNHAPPY_PATH(
+            device = Device.TABLET,
+            fontSize = FontSize.NORMAL,
+            locale = Locale.ARABIC,
+            uiMode = UiMode.DARK
+        )
+    }
+
+    @get:Rule(order = 0)
+    val fakeImageLoaderRule = FakeImageLoaderRule { getApplicationContext() }
+
+    @get:Rule(order = 1)
+    val addActivityToRobolectricRule =
+        AddActivityToRobolectricRule(FlavorshubActivity::class.java)
+
+    @get:Rule(order = 2)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 3)
+    val robolectricQualifiersRule = object : TestWatcher() {
+        override fun starting(description: Description) {
+            setQualifiers(testCase.device.qualifier)
+            setQualifiers(testCase.uiMode.cumulativeQualifier)
+            setQualifiers(testCase.locale.cumulativeQualifier)
+            setFontScale(testCase.fontSize.scale)
+        }
+    }
+
+    @get:Rule(order = 4)
+    val composeTestRule = createAndroidComposeRule<FlavorshubActivity>()
+
+    @get:Rule(order = 5)
+    val roborazziRule =
+        RoborazziRule(
+            composeRule = composeTestRule,
+            captureRoot = composeTestRule.onRoot(),
+            options = Options(
+                captureType = RoborazziRule.CaptureType.Gif()
+            )
+        )
+
+    @Test
+    fun multiFrameScreenshots() {
+        ScreenInteractor(
+            composeTestRule = composeTestRule,
+            parameterNames = listOf(
+                testCase.device,
+                testCase.uiMode,
+                testCase.locale,
+                testCase.fontSize
+            )
+        ).run {
+            captureScreenshot("1. Show Home")
+
+            addToFavoritesButtonInRecipeAtIndex(0).performClick()
+            captureScreenshot("2. Added 1st recipe to favorites")
+
+            bottomTabFavorites().performClick()
+            captureScreenshot("3. Show Favorites")
+
+            recipeImageAtIndex(0).performClick()
+            captureScreenshot("4. Show Details Screen of favorited Recipe")
+
+            pressBack()
+            captureScreenshot("5. Back in Favorites")
+
+            removeFromFavoritesButtonInRecipeAtIndex(0).performClick()
+            captureScreenshot("6. Removed favorited Recipe")
+        }
+    }
+}
