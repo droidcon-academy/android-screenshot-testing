197cd485bfd0e43b103af145491e124c4bbca968
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2025-05-31 10:20:50 +0200

    Add missing "Avoid Overtesting" Code challenge test classes in starter project

---
+++ source-code/starter-project/paparazzi-tests/src/test/java/com/droidcon/flavorshub/paparazzi/screenshots/NoOvertestingRecipeCardScreenshotTest.kt
@@ -0,0 +1,12 @@
+package com.droidcon.flavorshub.paparazzi.screenshots
+
+/**
+ * Execute the following command for this screenshot
+ * Record:
+ * ./gradlew :paparazzi-tests:recordPaparazziDebug --tests 'NoOvertestingRecipeCardScreenshotTest'
+ * Verify:
+ * ./gradlew :paparazzi-tests:verifyPaparazziDebug --tests 'NoOvertestingRecipeCardScreenshotTest'
+ */
+class NoOvertestingRecipeCardScreenshotTest {
+
+}
---
+++ source-code/starter-project/roborazzi-tests/src/test/java/com/droidcon/flavorshub/roborazzi/screenshots/NoOvertestingMultiFrameScreenshotTest.kt
@@ -0,0 +1,12 @@
+package com.droidcon.flavorshub.roborazzi.screenshots
+
+/**
+ * Execute the following command for this screenshot
+ * Record:
+ * ./gradlew :roborazzi-tests:recordRoborazziDebug --tests 'NoOvertestingMultiFrameScreenshotTest'
+ * Verify:
+ * ./gradlew :roborazzi-tests:verifyRoborazziDebug --tests 'NoOvertestingMultiFrameScreenshotTest'
+ */
+class NoOvertestingMultiFrameScreenshotTest {
+
+}
