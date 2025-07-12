f7ab5c0237fdb16757792ec4e62220b60de7adf4
Musab Nasredlin <musabagab2010@gmail.com>
2025-07-12 21:57:21 +0400

    Replace Green with Red color in FakeImageLoader for roborazzi

--- source-code/starter-project/roborazzi-tests/src/test/java/com/droidcon/flavorshub/roborazzi/screenshots/testrules/FakeImageLoader.kt
+++ source-code/starter-project/roborazzi-tests/src/test/java/com/droidcon/flavorshub/roborazzi/screenshots/testrules/FakeImageLoader.kt
@@ -3,7 +3,6 @@ package com.droidcon.flavorshub.roborazzi.screenshots.testrules
 import android.content.Context
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.test.core.app.ApplicationProvider
 import coil3.ColorImage
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
@@ -12,19 +11,13 @@ import coil3.annotation.ExperimentalCoilApi
 import coil3.test.FakeImageLoaderEngine
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import java.util.concurrent.atomic.AtomicInteger
 
 class FakeImageLoaderRule(val context: () -> Context) : TestWatcher() {
     @OptIn(ExperimentalCoilApi::class, DelicateCoilApi::class)
     override fun starting(description: Description?) {
         super.starting(description)
-        val requestCounter = AtomicInteger(0)
         val engine = FakeImageLoaderEngine.Builder()
-            .intercept(
-                { request -> requestCounter.getAndIncrement() == 0 },
-                ColorImage(Color.Red.toArgb())
-            )
-            .intercept({ request -> true }, ColorImage(Color.Green.toArgb()))
+            .intercept({ request -> true }, ColorImage(Color.Red.toArgb()))
             .build()
         val imageLoader = ImageLoader.Builder(context())
             .components { add(engine) }
